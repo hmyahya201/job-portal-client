59c83a61b6c8acb7ed6827c9d8294f1fd937589d
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-09-19 17:18:30 +0600

    commit responsive category section

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -13,25 +13,26 @@ import { BsPersonWorkspace } from "react-icons/bs";
 
 const Category = () => {
   return (
-    <div className="main-category-container p-5">
-      <h1 className="text-center p-5 text-white category-title">Category</h1>
-      <div className="category-container row ">
+    <div className="main-category-container pt-5 pb-5">
+      <h1 className="text-center pb-2 fw-semibold display-3 category-title">Category</h1>
+      <p className="category_sub-title mx-auto text-center fs-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, doloremque? Sapiente nam qui repudiandae accusamus</p>
+      <div className="category-container row mt-5">
         <div className="card">
           <img src="" alt="" />
-          <HiStatusOffline color="green" fontSize="4em" />
-          <h1>Offline</h1>
+          <HiStatusOffline className="category-icon" fontSize="4em" />
+          <h1 className="fs-2 category-heading">Offline</h1>
         </div>
         <div className="card">
-          <HiStatusOnline color="green" fontSize="4em" />
-          <h1>Remote</h1>
+          <HiStatusOnline className="category-icon" fontSize="4em" />
+          <h1 className="fs-2 category-heading">Remote</h1>
         </div>
         <div className="card">
-          <FaPhoenixFramework color="green" fontSize="4em" />
-          <h1>Part Time</h1>
+          <FaPhoenixFramework className="category-icon" fontSize="4em" />
+          <h1 className="fs-2 category-heading">Part Time</h1>
         </div>
         <div className="card">
-          <BsPersonWorkspace color="green" fontSize="4em" />
-          <h1>Full time</h1>
+          <BsPersonWorkspace className="category-icon" fontSize="4em" />
+          <h1 className="fs-2 category-heading">Full time</h1>
         </div>
       </div>
     </div>
